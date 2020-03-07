b5351bce9a5ee4625898d81d2533860749dff336
Vagner Braga <vagnerzolho@gmail.com>
2020-03-07 09:55:32 -0300

    * Ajuste na base par criar outras branch

--- src/Principal.java
+++ src/Principal.java
@@ -1,16 +1,7 @@
 public class Principal {
 
     public static void main(String[] args){
-
-        Group g = Group.newGroup("Nois");
-
-        g.add(new User("Vagner"));
-        g.add(new User("Zé coméia"));
-        g.add(new User("Zeca piolho"));
-
-        g.sendMsg("bem vindo ao grupo seus cambada de coxa colad");
-
-        g.myNotify();
+            System.out.println("deu certo");
     }
 
 
